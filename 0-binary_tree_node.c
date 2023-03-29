dc7c0e0b585be7265369f4364c77517bf5223b6e
frank-Mwangi <kungofrank@gmail.com>
2023-03-29 01:30:25 -0700

    Changing sandboxes

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a new binary tree node
+ * @parent: pointer to the parent node
+ * @value: data of the new node
+ *
+ * Return: pointer to the new node
+ *
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
